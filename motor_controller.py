9cc4a0ba7bedb3332d719802efa3dde0aeb87abd
Alexis AV <alexisvde1210@gmail.com>
2026-05-13 15:10:53 +0200

    Test_gesch-3

--- motor_controller.py
+++ motor_controller.py
@@ -39,7 +39,7 @@ class StepperWrapper:
         self.stepper = SmartStepper(stepPin=step, dirPin=direction, accelCurve='smooth2')
         self.micro = micro
         self.stepper.minSpeed = 5
-        self.stepper.maxSpeed = 150
+        self.stepper.maxSpeed = 100
         self.stepper.acceleration = 200
 
     @property
