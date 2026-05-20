efc0b3d137dc594b07c9f240562f003ace737e2c
Dario Flükiger <dario.fluekiger@students.fhnw.ch>
2026-05-20 13:59:35 +0200

    mission changes

--- missions.py
+++ missions.py
@@ -1,30 +1,31 @@
 # Mission table definitions for ROBOT_PROJECT
 
 missions = {
-    "p1": [
-        ("gun", 90),
+      "p1": [
+        ("gun", 45),
         ("turn", 90),    
         ("drive", 600, 600),  
-        ("gun", 45),          
+        ("gun", 10),          
         ("fire", 1),
+        ("turn", -2),  
         ("fire", 2),
         ("fire", 3),
-        ("fire", 4), 
+        ("turn", -2),
+        ("fire", 4),
         # Vor dem Fluss warten nach dem schiessen und neu ausrichten
-        ("turn", 90),
+        ("turn", -86),
         ("drive", 50, 50), 
-        ("drive", -50, -50),
-        ("turn", 180), 
-        ("drive", 350, 350),
-        ("turn", 90), 
-        #Vor der brücke warten
-        ("drive", 350, 350), 
-        ("turn", 90), 
-        ("drive", 350, 350), 
+("turn", 180),
+("drive", 70, 70),
+        ("drive", -350, -350),
         ("turn", -90), 
-        ("drive", 600, 600),      
-    ],
+        #Vor der brücke warten
+        ("drive", 1200, 1200), 
+("drive", -50, -50),
+("turn", 90),
+        ("drive", 350, 350), 
 
+                ],
     "p2": [
         ("drive", 100, 100),      # 100mm vorwärts
         ("turn", 360),            # 360° nach rechts drehen
